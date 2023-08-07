a8353bfce52604a9897669b0812c59200505c502
Shubham Mishra <shubhammishra1442@gmail.com>
2023-08-06 23:20:04 -0700

    init file added in this

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
-
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try :
+        raise Exception("we are testing the Exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing logging module")
+
     return "Starting Machine Learning Project Journey"
+logging.info("this is our logging info")
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 # here we are declaring the variables for setup the functions
 
@@ -8,20 +8,19 @@ PROJECTNAME = "Housing Predictor"
 VERSION = "0.0.1"
 AUTHOR = "Shubham Mishra"
 DESCRIPTION = "This is my machine learning project"
-PACKAGES = ["housing"]
 REQUIREMENT_FILE_NAME = "requirements.txt"
 
 # this function is going to return 
 
 def get_requirements_list()-> List[str]:
    with open(REQUIREMENT_FILE_NAME) as requirement_file:
-      return requirement_file.readlines()
+      return requirement_file.readlines().remove("-e .")
 
 setup(
     name= PROJECTNAME,
     version= VERSION,
     author= AUTHOR,
     description= DESCRIPTION,
-    packages= PACKAGES,
+    packages= find_packages(),
     install_requires = get_requirements_list()
 )
